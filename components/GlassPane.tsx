b3e22127f2028e55728a5e74a11e170c0e573ae3
hendo-getagent <hendo@getagent.co.uk>
2023-02-02 01:37:03 +0000

    did stuff

---
+++ components/GlassPane.tsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+
+export interface props {
+	children?: React.ReactNode
+	className?: React.ReactNode
+}
+
+const GlassPane = ({ children, className }: props) =>
+{
+	return <div className={clsx("glass rounded-2xl border-solid border-2 border-gray-200", className)}>
+		{children}
+	</div>;
+};
+
+export default GlassPane;
+
